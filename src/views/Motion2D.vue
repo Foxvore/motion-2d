3532a413b189b13186e76f96399c60f587f85979
“Foxvore” <imjohani2005@gmail.com>
2023-09-23 15:22:22 +0700

    feat: add half header & transition section

---
+++ src/views/Motion2D.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HalfHeader from "@/components/HalfHeader.vue";
+import Transisi from "@/components/motion-2d/Transisi.vue";
+</script>
+
+<template>
+  <section id="header">
+    <HalfHeader title="What is Motion 2D?" subtitle="Definition & Function" />
+  </section>
+
+  <section id="transition" class="px-[120px] my-[50px] h-auto">
+    <Transisi />
+  </section>
+
+  <div class="px-[70px]">
+    <hr />
+  </div>
+</template>
