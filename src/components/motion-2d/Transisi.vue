dae68dc8fbc554549e7f31911383c0958cf0f40f
“Foxvore” <imjohani2005@gmail.com>
2023-09-26 13:59:56 +0700

    feat: use transition example

--- src/components/motion-2d/Transisi.vue
+++ src/components/motion-2d/Transisi.vue
@@ -1,47 +1,57 @@
 <script setup>
+import Cut from "@/assets/gif/transisi/Cut.gif";
+import Fade from "@/assets/gif/transisi/Fade.gif";
+import Slide from "@/assets/gif/transisi/Slide.gif";
+import Zoom from "@/assets/gif/transisi/Zoom.gif";
+import Rotate from "@/assets/gif/transisi/Rotate.gif";
+import Flip from "@/assets/gif/transisi/Flip.gif";
+import Dissolve from "@/assets/gif/transisi/Dissolve.gif";
+import Masking from "@/assets/gif/transisi/Masking.gif";
+import Particle from "@/assets/gif/transisi/Particle.gif";
+
 const transisi = [
   {
-    example: "",
+    example: Cut,
     name: "Cut",
     desc: "Ini adalah jenis transisi yang paling sederhana. Saat satu adegan atau gambar tiba-tiba digantikan oleh adegan atau gambar berikutnya tanpa efek khusus.",
   },
   {
-    example: "",
+    example: Fade,
     name: "Fade",
     desc: "Aset yang satu muncul perlahan-lahan sementara aset lainnya menghilang perlahan-lahan. Ini menciptakan efek transisi yang lembut.",
   },
   {
-    example: "",
+    example: Slide,
     name: "Slide",
     desc: "Dalam gerakan slide, aset bergerak secara horizontal atau vertikal untuk memberikan perpindahan antara elemen yang satu dengan yang lainnya.",
   },
   {
-    example: "",
+    example: Zoom,
     name: "Zoom",
     desc: "Pada gerakan zoom, kamera atau pandangan pemirsa bergerak mendekat atau menjauh dari objek, menciptakan efek perubahan skala.",
   },
   {
-    example: "",
+    example: Rotate,
     name: "Rotate",
     desc: ": Aset dapat berputar untuk mengubah transisi dari satu elemen ke elemen lainnya. Ini sering digunakan untuk memberikan efek perubahan sudut pandang.",
   },
   {
-    example: "",
+    example: Flip,
     name: "Flip",
     desc: "Dalam gerakan flip, aset dapat diputar secara horizontal atau vertikal untuk menciptakan efek cermin atau pembalikan.",
   },
   {
-    example: "",
+    example: Dissolve,
     name: "Dissolve",
     desc: "Pada transisi dissolve, aset yang satu mulai memudar secara perlahan-lahan sementara aset yang lain muncul. Ini menciptakan efek transisi yang lembut.",
   },
   {
-    example: "",
+    example: Masking,
     name: "Masking",
     desc: "Dalam masking, elemen tertentu digunakan sebagai masker untuk mengungkap elemen di belakangnya secara perlahan-lahan. Ini sering digunakan untuk mengubah bentuk elemen.",
   },
   {
-    example: "",
+    example: Particle,
     name: "Particle Effects",
     desc: "Gerakan transisi dapat melibatkan efek partikel seperti percikan, debu, atau bintang yang muncul atau menghilang untuk menghubungkan dua elemen.",
   },
@@ -56,9 +66,9 @@ const transisi = [
   <div class="grid grid-flow-row grid-cols-3">
     <el-card v-for="item in transisi" :key="item" class="transition-card m-2">
       <div
-        class="w-full h-[250px] bg-gray-400 rounded-md mb-3 flex justify-center items-center"
+        class="w-full h-[250px] rounded-md mb-3 flex justify-center items-center"
       >
-        Contoh Transisi
+        <img :src="item.example" alt="" />
       </div>
       <p class="title text-2xl text-center">{{ item.name }}</p>
       <p class="text-justify p-3">{{ item.desc }}</p>
